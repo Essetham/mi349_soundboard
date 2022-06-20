8445cb7a2d22a23621ac49e0922921b0910aa8d8
Essetham <63083604+Essetham@users.noreply.github.com>
2022-06-20 13:09:17 +0800

    Update soundboard.js

--- soundboard.js
+++ soundboard.js
@@ -1,3 +1,17 @@
 var car = new Audio('Car.wav');
 var coin = new Audio('Coin.wav');
 var submarine = new Audio('Submarine.wav');
+
+function playCarSound(){
+   car.play();
+}
+
+
+function playCoinSound(){
+   coin.play();
+}
+
+
+function playSubmarineSound(){
+   submarine.play();
+}
